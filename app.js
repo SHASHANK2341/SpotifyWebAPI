24fda2039ceeed74320978cfac2333d6a5d7745b
SHASHANK2341 <84630088+SHASHANK2341@users.noreply.github.com>
2021-10-15 19:38:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var redirect_uri = "https://makeratplay.github.io/SpotifyWebAPI/"; // change this your value
+var redirect_uri = "https://shashank2341.github.io/SHASHANK2341-SpotifyApi//"; // change this your value
 //var redirect_uri = "http://127.0.0.1:5500/index.html";
  
 
